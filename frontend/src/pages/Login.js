0b494d5d43b794807109f4ae376ab03d1d20f06c
Shivam <122827835+Shivaam16@users.noreply.github.com>
2025-04-29 15:49:36 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/login`;
+            const url = `https://deploy-mern-app-api-orpin.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
