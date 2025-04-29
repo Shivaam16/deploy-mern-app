5bde668a531db46c3494a12e1b18bc85e306a12e
Shivam <122827835+Shivaam16@users.noreply.github.com>
2025-04-29 15:50:10 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8080/auth/product";
+      const url = "https://deploy-mern-app-api-orpin.vercel.app/auth/product";
       const headers = {
         headers: {
           Authorization: localStorage.getItem("token"),
